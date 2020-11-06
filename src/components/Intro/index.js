6e7eb5a87fc4c0433183f3e3b5717528cfde74c5
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-05 23:01:17 -0800

    adding intro section

---
+++ src/components/Intro/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './index.css';
+
+function Intro() {
+  return (
+    <div>
+      <h1 class="display-4" id="greeting">Hi there!</h1>
+      <p class="lead">My name is <strong>Melinda Ivanov</strong>, nice to meet you.</p>
+      <p>I am a technical-minded developer leveraging an MSc degree in earth sciences from
+      University of Szeged while currently working towards a full-stack web development
+      certificate at University of Washington. My experience includes taking responsibilities
+      in design, technical, and development decisions in academic projects using newly
+      developed skills in Java backend and JavaScript full stack. As a passionate problem
+      solver, my aim is to deliver quality products by applying clean design/coding principles
+      and following agile methodologies. I enjoy creating user-friendly applications while
+      utilizing both my engineering and science qualifications. I am a fast learner who is
+      enthusiastic about innovative techniques and excited to leverage my skills as part of a
+                                fast-paced, quality-driven team.</p>
+    </div>
+  );
+}
+
+export default Intro;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Intro from '../components/Intro';
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
-      <div>
-        home
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <div class="row" id="intro">
+            <div class="col-sm-7">
+              <Intro></Intro>
+            </div>
+            <div class="col-sm-5">
+              <img src="assets/images/profile.jpg" alt="Profile" class="center" id="profileImg"></img>
+
+
+              <div class="row">
+                <h2 class="display-4" id="contact">More about me:</h2>
+              </div>
+              <div class="row">
+                {/* <a href="https://www.linkedin.com/in/melinda-ivanov/" id="linkedin" class="fa fa-linkedin fa-4x" data-toggle="tooltip" data-placement="right" title="Linkedin"></a>
+                <a href="https://github.com/jnsmelinda" class="fa fa-github fa-4x" data-toggle="tooltip" data-placement="right" title="GitHub" id="github"></a>
+                <a href="assets/cv/resume.pdf" class="fa fa-file-pdf-o fa-4x" data-toggle="tooltip" data-placement="right" title="Resume" id="resume"></a> */}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer></Footer>
     </div>
