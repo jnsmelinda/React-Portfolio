2d9080bd785e58db591fc809dc2e4c74a8ccabc5
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-05 22:42:03 -0800

    creating header

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Header() {
-  return (
-    <div>header component</div>
-  );
-}
-
-export default Header;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './index.css';
+
+function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <Link to="home" className="logo">MI</Link>
+        <ul>
+          <li><Link to="home" id="link">Home</Link></li>
+          <li><Link to="contact" id="link">Contact</Link></li>
+          <li><Link to="Projects" id="link">Projects</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
